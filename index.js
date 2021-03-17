f43c663f262c7405c60b245118126e76c97dd02b
Xiaochen Xu <xiaochex@gmail.com>
2021-03-16 21:31:13 -0400

    commit tree 9d9d13c7258168c7c24fc56ed9728819a0158199

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+    
+  }
+
+  function mondayWork(activity = "go to the office"){
+      return `This Monday, I will ${activity}.`
+
+  }
+function wrapAdjective(adjective = "*"){
+     return function(compliment = "special"){
+         return `You are ${adjective}${compliment}${adjective}!`
+
+     }
+
+}
+wrapAdjective()
